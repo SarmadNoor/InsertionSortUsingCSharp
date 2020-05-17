03c6d43daaf3e71e2b6bbe1f7468243d9855120b
SarmadNoor <43487713+SarmadNoor@users.noreply.github.com>
2020-05-17 15:04:35 -0400

    Initial commit for Insetion sort

---
+++ InsertionSortUsingCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InsertionSortUsingCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insertion Sort begins!");
+        }
+    }
+}
