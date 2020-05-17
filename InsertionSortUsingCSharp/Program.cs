cd3de3f7b3a3b58dc055fd9e0500f57dddea98bb
SarmadNoor <43487713+SarmadNoor@users.noreply.github.com>
2020-05-17 16:03:01 -0400

    insertion sort algorithm

--- InsertionSortUsingCSharp/Program.cs
+++ InsertionSortUsingCSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers;
 
 namespace InsertionSortUsingCSharp
 {
@@ -6,7 +7,37 @@ namespace InsertionSortUsingCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Insertion Sort begins!");
+            int[] unsortedArray = { 41, 31, 59, 26, 48, 37 };
+            Console.WriteLine("Unsorted array");
+            foreach (int unSortedElement in unsortedArray)
+            {
+                Console.WriteLine(unSortedElement);
+            }
+            Console.WriteLine("Insertion Sort begins !");
+            int[] sortedArray = InsertionSortAlgorithm(unsortedArray);
+            Console.WriteLine("Insertion sort ends !");
+            Console.WriteLine("Sorted Array");
+            foreach (int sortedElement in sortedArray)
+            {
+                Console.WriteLine(sortedElement);
+            }
+            Console.ReadKey();
         }
+        private static int[] InsertionSortAlgorithm(int[] unsortedArray)
+        {
+            for(int j=1; j < unsortedArray.Length; j++)
+            {
+                int key = unsortedArray[j];
+                int i = j-1;
+                while( i >= 0 && unsortedArray[i] > key)
+                {
+                    unsortedArray[i + 1] = unsortedArray[i];
+                    i--;
+                }
+                unsortedArray[i+1] = key;
+            }
+            return unsortedArray;
+        }
+
     }
 }
